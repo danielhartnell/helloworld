3aa60102f488f517e1a384fc7e66ce08f671ebbb
Daniel Hartnell <danielhartnell@gmail.com>
2018-07-10 15:45:16 -0700

    testing webhook trigger from master

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Kubernetes still rocks from master!\n")
+		fmt.Fprintf(w, "Kubernetes still rocks from master (v2)!\n")
                 fmt.Println("User requested: /")
 	})
 
