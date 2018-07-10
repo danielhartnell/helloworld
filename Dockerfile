209ab7811d82875469d37d6842e9b3b0255cee47
Daniel Hartnell <danielhartnell@gmail.com>
2018-07-10 10:24:45 -0700

    adding Prometheus metrics endpoint

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM golang:latest
 RUN mkdir /app 
 ADD . /app/ 
 WORKDIR /app 
+RUN go get github.com/prometheus/client_golang/prometheus/promhttp
 RUN go build -o main . 
 CMD ["/app/main"]
--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Kubernetes still rocks\n")
-                fmt.Println("Successfully responded to internet citizen")
+                fmt.Println("User requested: /")
 	})
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":8080", nil)
 }
